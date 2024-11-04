deedd62ef52d3edaa0f9af5f1ec599c9f3497cbf
KeiNagaoka <kei20000809@gmail.com>
2024-11-05 01:06:12 +0900

    Update settings.py

--- routingSystem/settings.py
+++ routingSystem/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+h!dopmje_!nwkzv@6sq@tc#q$*t(%(5kwq$g!b95q51y)c@u3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['routing-system-b99f58cf3915.herokuapp.com','localhost','127.0.0.1']
 # ALLOWED_HOSTS = ['*']
